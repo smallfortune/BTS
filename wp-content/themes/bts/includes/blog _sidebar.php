95853c7929824a9a21e8f57f54c28d41586c457e
Parker <parker@smlfrtn.com>
2011-12-13 15:20:08 -0500

    got sidebars html in also got the twitter plugin in there. started on the footer styles

---
+++ wp-content/themes/bts/includes/blog _sidebar.php
@@ -0,0 +1,6 @@
+
+ <div id="social_sidebar" class="sidebar">
+    <ul>
+        <li>test</li>
+    </ul>
+</div>
--- wp-content/themes/bts/news-page.php
+++ wp-content/themes/bts/news-page.php
@@ -15,57 +15,85 @@ Template Name: News
  */
 
 get_header(); ?>
+
 <div id="primary">
     <div id="content" role="main">
-<?php query_posts('post_type=post');?>
-<?php if (have_posts()) : ?>
+   
+    <?php if (have_posts()) : ?>
+        <?php query_posts('post_type=post&cat=-9');?>
+        <?php while (have_posts()) : the_post(); ?>
+            <div class="post-news">
+                <div class="top-news">
+                    <h2><?php the_title(); ?></h2>
+                </div>
+                <?php //the_content( 'Read the full post»' ); ?>
+                <?php if(has_post_thumbnail()):?>
+                <div class="thumb">
+                    <a href="<?php the_permalink();?>"><?php the_post_thumbnail('medium');?></a>
+                </div>
+                <?php endif;?>
+                <div class="excerpt">
+                    <?php the_excerpt('Read the full post»' );?>
+                </div>
+            </div>
+            <div class="post-footer">
+                <a href="<?php the_permalink(); ?>" title="read more" class="read_more_news"></a>
+                <div class="social">
+                    <p class="date"><?php the_time('F jS, Y') ?></p>
+                    <script charset="utf-8" type="text/javascript">var switchTo5x=true;</script><script charset="utf-8" type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script><script type="text/javascript">stLight.options({publisher:'wp.e5e164fb-0493-4667-b8f5-468dd1e6d80f'});var st_type='wordpress3.2.1';</script>
+                </div>
+            </div>
+        <?php endwhile;?>
+        <?php else : ?>
+            <article id="post-0" class="post no-results not-found">
+                <header class="entry-header">
+                    <h1 class="entry-title">Nothing Found</h1>
+                </header><!-- .entry-header -->
 
+                <div class="entry-content">
+                    <p>Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.</p>
+                    <?php get_search_form(); ?>
+                </div><!-- .entry-content -->	                                
+            </article><!-- #post-0 -->
+        <?php endif; ?>
+        </div><!-- #content -->
+    </div><!-- #primary -->
+    <?php rewind_posts(); ?>
+    <div id="social_sidebar" class="sidebar">
+        <h2>Subscribe to BTS</h2>
+        <ul>
+            <li><a href="#" title="" id="feed_twitter">Follow Us @BizTravSuccess</a></li>
+            <li><a href="#" title="" id="feed_rss">Get Our RSS Feed</a></li>
+            <li><a href="#" title="" id="feed_facebook">Like Us on Facebook</a></li>
+        </ul>
+    </div>
+    <div class="sidebar">
+        <h2>Categories</h2>
+        <?php 
+          $categories=  get_categories(); 
+          foreach ($categories as $category) {
+                $li = '<li>';
+                $li .= '<a href='. get_category_link( $category->term_id ) .'>';
+                $li .= $category->cat_name;
+                $li .= '</a>';
+                $li .= '</li>';
+                echo $li;
+          }
+         ?>
+        
+    </div>
+    <div class="sidebar">
+        <h2>Follow Us on Twitter</h2>
+        <?php rewind_posts();?>
+        <?php if (have_posts()) : ?>
+        <?php query_posts('post_type=post&cat=9');?>
+        <?php while (have_posts()) : the_post(); ?>
+            <?php the_content();?>
+        <?php endwhile;?>
+        <?php else:?>
+            <p>no tweets today</p>
+        <?php endif;?>
+    </div>
     
-
-    
-    <?php while (have_posts()) : the_post(); ?>
-        <div class="post-news">
-            <div class="top-news">
-                <h2><?php the_title(); ?></h2>
-            </div>
-            <?php //the_content( 'Read the full post»' ); ?>
-            <?php if(has_post_thumbnail()):?>
-            <div class="thumb">
-                <a href="<?php the_permalink();?>"><?php the_post_thumbnail('medium');?></a>
-            </div>
-            <?php endif;?>
-            <div class="excerpt">
-                <?php the_excerpt('Read the full post»' );?>
-            </div>
-            
-        </div>
-        <div class="post-footer">
-            <a href="<?php the_permalink(); ?>" title="read more" class="read_more_news"></a>
-            <div class="social">
-                <p class="date"><?php the_time('F jS, Y') ?></p>
-                <script charset="utf-8" type="text/javascript">var switchTo5x=true;</script><script charset="utf-8" type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script><script type="text/javascript">stLight.options({publisher:'wp.e5e164fb-0493-4667-b8f5-468dd1e6d80f'});var st_type='wordpress3.2.1';</script>
-            </div>
-        </div>
-    <?php endwhile;?>
-
-    
-
-<?php else : ?>
-
-    <article id="post-0" class="post no-results not-found">
-        <header class="entry-header">
-            <h1 class="entry-title">Nothing Found</h1>
-        </header><!-- .entry-header -->
-
-        <div class="entry-content">
-            <p>Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.</p>
-            <?php get_search_form(); ?>
-        </div><!-- .entry-content -->	                                
-    </article><!-- #post-0 -->
-
-<?php endif; ?>
-</div><!-- #content -->
-</div><!-- #primary -->
-
 
 <?php get_footer(); ?>
